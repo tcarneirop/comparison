64ae715a784d1bc0233fd400464f35b4e43934c2
Tiago Carneiro <carneirouece@gmail.com>
2022-06-21 17:44:00 +0200

    preparing the partial search for implementing the parallel one

--- julia/complete.jl
+++ julia/complete.jl
@@ -1,67 +1,66 @@
-let
 
-__VOID__     = 0 
-__VISITED__    = 1
-__N_VISITED__   = 0
-size   = 13
+function complete_enumeration(size)
 
-#zeros(Int8,5)
+	__VOID__     = 0
+	__VISITED__    = 1
+	__N_VISITED__   = 0
 
+	#obs: because the vector begins with 1 I need to use size+1 for N-Queens of size 'size'
+	print("Complete enumeration of a permutation of size ")
+	println(size-1)
 
-depth = 1
-tree_size = 0
-number_of_solutions = 0
-local_visited = zeros(Int64,size+2)
-local_cycle = zeros(Int64,size+2)
+	depth = 1
+	tree_size = 0
+	number_of_solutions = 0
+	local_visited = zeros(Int64,size)
+	local_permutation = zeros(Int64,size)
 
-#fill!(local_visited, 0)
-#fill!(local_cycle, -1)
+	println(local_visited)
+	println(local_permutation)
 
+	@time begin
+	while true
+		#%println(local_cycle)
 
-println(local_visited)
-println(local_cycle)
+		local_permutation[depth] = local_permutation[depth]+1
 
-println("Starting")
+		if local_permutation[depth] == (size+1)
+			local_permutation[depth] = __VOID__
+		else
+			if (local_visited[local_permutation[depth]] == 0)
 
-println(size-1)
+				local_visited[local_permutation[depth]] = __VISITED__
+				depth +=1
+				tree_size+=1
 
-@time begin
-while true
-
-	#println(local_cycle)
-	
-	local_cycle[depth] = local_cycle[depth]+1
-
-	#println(local_cycle)
-
-	if local_cycle[depth] == (size+1)
-		local_cycle[depth] = __VOID__
-
-	elseif (local_visited[local_cycle[depth]] == 0 ) 
-			
-			local_visited[local_cycle[depth]] = __VISITED__
-			depth +=1
-			tree_size+=1
-			
-			if depth == size+1
-				number_of_solutions+=1
+				if depth == size+1 ##complete solution -- full and valid permutation
+					number_of_solutions+=1
+					#my_print(local_cycle)
+				else
+					continue
+				end
 			else
 				continue
-			end 
-		else
-			continue
-	end
+			end #elif
+		end
 
-	depth -= 1
-	local_visited[local_cycle[depth]] = __N_VISITED__
-	
-	if depth < 2 
-		break
+		depth -= 1
+		local_visited[local_permutation[depth]] = __N_VISITED__
+
+		if depth < 2
+			break
+		end #if depth<2
+
+
 	end
-end
 end
 
 println("Number of solutions: ")
 println(number_of_solutions)
 println(tree_size)
-end
+end #queens serial
+
+
+println(ARGS)
+size = parse(Int64,ARGS[1])
+complete_enumeration(size+1)
--- julia/queensSerial.jl
+++ julia/queensSerial.jl
@@ -1,9 +1,25 @@
 
-#Need to update it printing from the second to the last one
-function my_print(x::Union{Number, AbstractArray{<:Number}})
-  println("value = ", x)
+
+##@TODO: Queens -- we receive the three one unity bigger than it should be. Lets see this problem afterwards.
+###
+struct Subproblem
+
+	subproblem_is_visited::Array{Int64,1}
+ 	subproblem_partial_permutation::Array{Int64,1}
+
+
+	setVisited(visited_::Array{Int64,1}) = ( subproblem_is_visited = visited_)
+	setPermutation(permutation_::Array{Int64,1}) = (	subproblem_partial_permutation = permutation_)
+
+
 end
 
+struct Metrics
+
+  	number_of_solutions::Int64
+	partial_tree_size::Int64
+
+end
 
 #verifies whether a given solution/incomplete solution is feasible
 function queens_is_valid_configuration(board::Union{Number, AbstractArray{<:Number}}, roll)::Bool
@@ -27,6 +43,7 @@ function queens_is_valid_configuration(board::Union{Number, AbstractArray{<:Numb
 
 	return true
 end ##queens_is_valid_conf
+
 
 
 function queens_serial(size)
@@ -91,20 +108,23 @@ println(tree_size)
 end #queens serial
 
 
-
-function complete_enumeration(size)
+function queens_partial_search(size, cutoff_depth, subproblems_pool::AbstractArray{Subproblem})::Metrics
 
 	__VOID__     = 0
 	__VISITED__    = 1
 	__N_VISITED__   = 0
 
 	#obs: because the vector begins with 1 I need to use size+1 for N-Queens of size 'size'
-	print("Complete enumeration of a permutation of size ")
+	print("Starting N-Queens of size ")
 	println(size-1)
+	print("Partial search until cutoff ")
+	println(cutoff_depth)
+
 
 	depth = 1
 	tree_size = 0
-	number_of_solutions = 0
+	number_of_subproblems = 0
+
 	local_visited = zeros(Int64,size)
 	local_permutation = zeros(Int64,size)
 
@@ -113,21 +133,22 @@ function complete_enumeration(size)
 
 	@time begin
 	while true
-		#%println(local_cycle)
-
+		#%println(local_cycle
 		local_permutation[depth] = local_permutation[depth]+1
 
 		if local_permutation[depth] == (size+1)
 			local_permutation[depth] = __VOID__
 		else
-			if (local_visited[local_permutation[depth]] == 0)
+			if (local_visited[local_permutation[depth]] == 0 && queens_is_valid_configuration(local_permutation,depth))
 
 				local_visited[local_permutation[depth]] = __VISITED__
 				depth +=1
 				tree_size+=1
 
-				if depth == size+1 ##complete solution -- full and valid permutation
-					number_of_solutions+=1
+				if depth == cutoff_depth ##complete solution -- full, feasible and valid solution
+					number_of_subproblems+=1
+					subproblems[number_of_subproblems].setPermutation(local_permutation)
+					subproblems[number_of_subproblems].setVisited(local_visited)
 					#my_print(local_cycle)
 				else
 					continue
@@ -140,12 +161,17 @@ function complete_enumeration(size)
 		depth -= 1
 		local_visited[local_permutation[depth]] = __N_VISITED__
 
-		if depth < 2
+		if depth < cutoff_depth
 			break
 		end #if depth<2
-
-
 	end
+
+	println(subproblems)
+	metrics = Metrics(tree_size , number_of_subproblems)
+	println(metrics)
+	return metrics
+
+
 end
 
 println("Number of solutions: ")
@@ -154,8 +180,10 @@ println(tree_size)
 end #queens serial
 
 
-println(ARGS[1])
-println(typeof(ARGS[1]))
+println(ARGS)
 size = parse(Int64,ARGS[1])
-queens_serial(size+1)
-#complete_enumeration(size+1)
+cutoff_depth = parse(Int64, ARGS[2])
+
+subproblems = Array{Subproblem, 1}(undef, 99999)
+
+queens_partial_search(size+1,cutoff_depth,subproblems)
