8d62a37fcda0e63e3f919ba2db442f059316c301
tcarneirop <carneirouece@gmail.com>
2022-10-17 17:08:13 +0200

    looks like the mcpu mcore code in julia is done -- not tested....

--- julia/queensCUDA.jl
+++ julia/queensCUDA.jl
@@ -108,6 +108,7 @@ function queens_sgpu_caller(::Val{size}, ::Val{cutoff_depth}, ::Val{__BLOCK_SIZE
 	#end of the partial search
 
 	number_of_solutions = 0
+	partial_tree_size = 0
 	#metrics.number_of_solutions = 0
 
 	indexes_h = subpermutation_h = zeros(Int32, number_of_subproblems)
@@ -157,6 +158,84 @@ end #caller
 
 
 
+#####Obs::: wrong here...
+function queens_mcpu_mgpu_subproblems_organizer!(cutoff_depth, num_subproblems, prefixes, controls,subproblems, starting_point)
+
+	for sub in 0:num_subproblems-1
+		stride = sub*cutoff_depth
+		for j in 1:cutoff_depth
+			prefixes[stride+j] = subproblems[sub+starting_point][2][j] # subproblem_partial_permutation
+			controls[stride+j] = subproblems[sub+starting_point][1][j] # subproblem_is_visited
+		end
+	end
+
+end
+
+
+#function queens_mcpu_mgpu_gpu_caller(::Val{size}, ::Val{cutoff_depth}, 
+#	::Val{__BLOCK_SIZE_}, ::Val{number_of_subproblems}, ::Val{starting_point},
+#	::Val{device}, ::Vector{subproblems}) where {size, cutoff_depth, __BLOCK_SIZE_,
+#	number_of_subproblems, starting_point, device, subproblems}
+function queens_mcpu_mgpu_gpu_caller(size, cutoff_depth, 
+	__BLOCK_SIZE_, number_of_subproblems, starting_point,
+	device, subproblems)
+
+	#__BLOCK_SIZE_ = 1024
+
+	print("Starting single-GPU-based N-Queens of size ", size-1, " Device: ",  )
+	println(size-1)
+
+	#subproblems = [Subproblem(size) for i in 1:1000000]
+
+	
+	number_of_solutions = 0
+	partial_tree_size = 0
+	#metrics.number_of_solutions = 0
+
+	indexes_h = subpermutation_h = zeros(Int32, number_of_subproblems)
+	subpermutation_h = zeros(Int64, cutoff_depth*number_of_subproblems)
+	controls_h = zeros(Int64, cutoff_depth*number_of_subproblems)
+	number_of_solutions_h = zeros(Int64, number_of_subproblems)
+	tree_size_h = zeros(Int64, number_of_subproblems)
+
+	queens_mcpu_mgpu_subproblems_organizer!(cutoff_depth, number_of_subproblems, subpermutation_h, controls_h, subproblems, starting_point)
+
+	#### the subpermutation_d is the memory allocated to keep all subpermutations and the control vectors...
+	##### Maybe I could have done it in a smarter way...
+	subpermutation_d      = CuArray{Int64}(undef,  cutoff_depth*number_of_subproblems)
+	controls_d            = CuArray{Int64}(undef,  cutoff_depth*number_of_subproblems)
+
+	#### Tree size and number of solutions is to get the metrics from the search.
+	indexes_d = CUDA.zeros(Int32,number_of_subproblems)
+	number_of_solutions_d = CUDA.zeros(Int64, number_of_subproblems)
+	tree_size_d = CUDA.zeros(Int64,number_of_subproblems)
+
+	# copy from the CPU to the GPU
+	copyto!(subpermutation_d, subpermutation_h)
+	# copy from the CPU to the GPU
+	copyto!(controls_d, controls_h)
+
+	num_blocks = ceil(Int, number_of_subproblems/__BLOCK_SIZE_)
+
+	@info "Number of subproblems:", number_of_subproblems, " - Number of blocks:  ", num_blocks
+
+    @cuda threads=__BLOCK_SIZE_ blocks=num_blocks gpu_queens_tree_explorer!(Val(size),Val(cutoff_depth), Val(number_of_subproblems), subpermutation_d, controls_d, tree_size_d, number_of_solutions_d, indexes_d)
+
+    #from de gpu to the cpu
+	copyto!(number_of_solutions_h, number_of_solutions_d)
+	#from de gpu to the cpu
+	copyto!(tree_size_h, tree_size_d)
+
+	copyto!(indexes_h, indexes_d)
+	number_of_solutions = sum(number_of_solutions_h)
+	partial_tree_size += sum(tree_size_h)
+
+	#print("\n\n")
+	#print(indexes_h)
+
+	println("\n###########################")
+	println("N-Queens size: ", size-1, "\n###########################\n" ,"\nNumber of sols: ",number_of_solutions, "\nTree size: " ,partial_tree_size,"\n\n")
+end #caller
 
 
 function get_cpu_load(percent::Float64, num_subproblems::Int64)::Int64
@@ -173,6 +252,20 @@ function get_load_each_gpu(gpu_load::Int64, num_gpus::Int64, device_load )
 		end
 	end
 
+end ###
+
+
+function get_starting_point_each_gpu(cpu_load::Int64, num_devices, device_load,device_starting_point)
+	starting_point = cpu_load
+	
+	if(num_devices == 1)
+		device_starting_point[1] = starting_point +1
+	else
+		for device in 2:num_devices
+			starting_point += device_load[device]
+			device_starting_point[device] = starting_point + 1
+		end
+	end
 end ###
 
 
@@ -206,8 +299,10 @@ function queens_mgpu_mcore_caller(::Val{size}, ::Val{cutoff_depth}, ::Val{__BLOC
 	cpu_load = get_cpu_load(cpup, number_of_subproblems)
     gpu_load = number_of_subproblems - cpu_load
     device_load = zeros(Int64, num_gpus)
-    get_load_each_gpu(gpu_load, num_gpus, device_load)
-
+    device_starting_position = zeros(Int64, num_gpus)
+	get_load_each_gpu(gpu_load, num_gpus, device_load)
+	get_starting_point_each_gpu(cpu_load, num_gpus, device_load, device_starting_position)
+	
 
     println("\nTotal load: ",number_of_subproblems , "\nTotal CPU load: ", cpu_load ,"  - CPU percent: ", cpup , " - GPU load: ", gpu_load);
     println("\nLoad of each GPU: ");
@@ -217,11 +312,13 @@ function queens_mgpu_mcore_caller(::Val{size}, ::Val{cutoff_depth}, ::Val{__BLOC
 
 	@sync begin
 		
-		if num_gpus>0
+		if num_gpus>0 && gpu_load>0
 			for gpu_dev in 1:num_gpus
 				@async begin
-					device!(gpu_dev)
-					println("gpu: ", gpu_dev)
+					device!(gpu_dev-1)
+					println("gpu: ", gpu_dev-1)
+					queens_mcpu_mgpu_gpu_caller(size, cutoff_depth, __BLOCK_SIZE_, device_load[gpu_dev],
+					device_starting_position[gpu_dev], gpu_dev, subproblems)
 					# do work on GPU 0 here
 				end
 			end
